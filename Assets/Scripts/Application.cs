bb5f1bfeb5819b2fc637d032c30480b5a0bd421c
Bence Janos Szabo <szbence@hotmail.com>
2016-06-11 12:03:05 +0200

    New prefabs

--- Assets/Scripts/Application.cs
+++ Assets/Scripts/Application.cs
@@ -4,9 +4,14 @@ using System.Collections;
 public class Application : MonoBehaviour {
 
 	public GameObject gameManager;
+	public GameObject eventSystem;
+	public GameObject canvas;
 
 	// Use this for initialization
 	void Awake () {
+		Instantiate (eventSystem);
+		Instantiate (canvas); // TODO: make canvas the child of camera
+		//canvas.transform.parent = transform;
 		if (GameManager.instance == null) {
 			Instantiate (gameManager);
 		}
